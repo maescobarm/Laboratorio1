480f76f149a9c0362e2ab596aa7f7af5e4deb179
Marcelo Escobar <maescobarm@gmail.com>
2015-09-30 02:22:48 -0300

    Comit1

---
+++ Servidor-RMI/src/rmi/Implementacion.java
@@ -0,0 +1,64 @@
+package rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import rmi_interface.Interface;
+
+/**
+ *
+ * @author Daniel Wladdimiro Cottet
+ * @title Taller de sistemas distribuidos - Clase 1
+ */
+
+public class Implementacion extends UnicastRemoteObject implements Interface {
+    
+    static ArrayList<String> usuarios;
+    static ArrayList<String> rrhh;   
+    static ArrayList<String> finanzas;
+    
+    static Logger logger;
+
+    public Implementacion() throws RemoteException {
+        logger = Logger.getLogger(getClass().getName());
+        logger.log(Level.INFO, "Se ha instanciado la clase de Implementacion del Servidor");
+        usuarios = new ArrayList<>();
+        rrhh = new ArrayList<>();
+        finanzas = new ArrayList<>();
+    }
+
+    /*
+     * Debo escribir todos los métodos que se encuentran en la interface
+     */
+    // Por cada metodo se escribe Override que se utiliza para que utilize este metodo en vez del metodo del padre
+    @Override
+    public boolean borrarrrhh(String usuario) throws RemoteException {
+        logger.log(Level.INFO, "Se desea eliminar un usuario al servidor");
+        if(rrhh.contains(usuario)){
+            rrhh.remove(usuario);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    @Override
+    public boolean ingresarrrhh(String usuario) throws RemoteException {
+        logger.log(Level.INFO, "Se desea ingresar un usuario al servidor");
+        if(!rrhh.contains(usuario)){
+            rrhh.add(usuario);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public ArrayList<String> verUsuarios() throws RemoteException {
+        logger.log(Level.INFO, "Se desea ver los usuarios del servidor");
+        return rrhh;
+    }
+
+}
---
+++ Servidor-RMI/src/rmi_interface/Interface.java
@@ -0,0 +1,26 @@
+
+package rmi_interface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel Wladdimiro Cottet
+ * @title Taller de sistemas distribuidos - Clase 1
+ */
+
+
+//Esta inteface indicará los métodos que están a dispoción del cliente y servidor
+//para que puedan interactuar remotamente.
+//Todos estos métodos deben poseer como mínimo la excepción RemoteException
+
+
+public interface Interface extends Remote {
+    
+    public boolean ingresarrrhh(String usuario) throws RemoteException;
+    public boolean borrarrrhh(String usuario) throws RemoteException;
+          
+    public ArrayList<String> verUsuarios() throws RemoteException;
+}
